88074097006a68cb957620e9712fb67c2fd3efbb
ravipandya15 <ravipandya151298@gmail.com>
2021-02-11 20:17:35 +0530

    updated weather forecast controller

--- app1/Controllers/WeatherForecastController.cs
+++ app1/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace app1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
