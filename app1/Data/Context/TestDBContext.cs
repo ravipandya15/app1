657a1a3c8a829ff7150e7641612ca5f068b50e1c
ravipandya15 <ravipandya151298@gmail.com>
2021-02-12 18:25:26 +0530

    created API to get Employee List

---
+++ app1/Data/Context/TestDBContext.cs
@@ -0,0 +1,18 @@
+﻿using app1.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app1.Data.Context
+{
+    public class TestDBContext : DbContext
+    {
+        public TestDBContext(DbContextOptions<TestDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
--- app1/Startup.cs
+++ app1/Startup.cs
@@ -1,7 +1,10 @@
+using app1.Data;
+using app1.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,12 +23,17 @@ namespace app1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<TestDBContext>(options =>
+            //            options.UseSqlServer(Configuration.GetConnectionString("TestDBConnection")));
+
+            //services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            ApplicationExtension.RegisterApplicationDependencies(services, this.Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
